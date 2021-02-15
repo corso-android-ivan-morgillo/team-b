72513a4a1b720941a2ff6962d2973fd9f8707230
Domenico Savino <domenico.savino89@libero.it>
2021-02-15 20:32:21 +0100

    feat: added Detail Network call

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CocktailRepository.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CocktailRepository.kt
@@ -1,7 +1,9 @@
 package com.ivanmorgillo.corsoandroid.teamb
 
 import com.ivanmorgillo.corsoandroid.teamb.network.CocktailAPI
+import com.ivanmorgillo.corsoandroid.teamb.network.Ingredient
 import com.ivanmorgillo.corsoandroid.teamb.network.LoadCocktailResult
+import com.ivanmorgillo.corsoandroid.teamb.network.LoadDetailCocktailResult
 
 /* Interfaccia Repository Pattern
  * tipo suspend --> rappresenta un'operazione di input output --> è un'operazione che può fallire
@@ -9,6 +11,7 @@ import com.ivanmorgillo.corsoandroid.teamb.network.LoadCocktailResult
  * bisogna quindi segnalare agli utilizzatori che questa funzione sta facendo IO e che può crashare o durare tanto */
 interface CocktailRepository {
     suspend fun loadCocktails(): LoadCocktailResult
+    suspend fun loadDetailCocktails(cocktailId: Long): LoadDetailCocktailResult
 }
 
 /* Implementazione dell'interfaccia definita prima */
@@ -16,7 +19,22 @@ class CocktailRepositoryImpl(private val api: CocktailAPI) : CocktailRepository 
     override suspend fun loadCocktails(): LoadCocktailResult {
         return api.loadCocktails()
     }
+
+    override suspend fun loadDetailCocktails(cocktailId: Long): LoadDetailCocktailResult {
+        return api.loadDetailCocktails(cocktailId)
+    }
 }
 
 /* rappresenta una lista di ricette: lista di oggetti con un nome, immagine e id */
 data class Cocktail(val name: String, val image: String, val idDrink: Long)
+
+data class Detail(
+    val name: String,
+    val image: String,
+    val idDrink: Long,
+    val isAlcoholic: Boolean,
+    val glass: String,
+    val ingredients: List<Ingredient>,
+    val youtubeLink: String?,
+    val instructions: String,
+)
