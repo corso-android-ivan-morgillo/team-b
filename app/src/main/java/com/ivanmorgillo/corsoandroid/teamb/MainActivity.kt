a8db488ac3627413552eccf24324f31503b5474b
Domenico Savino <domenico.savino89@libero.it>
2021-03-01 15:13:14 +0100

    feat: set NavigationDrawer

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/MainActivity.kt
@@ -11,17 +11,56 @@ import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.navigation.Navigation
+import androidx.core.view.GravityCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
     private val mainActivityViewModel: MainActivityViewModel by viewModel()
-    private val navController by lazy { Navigation.findNavController(this, R.id.nav_host_fragment) }
+    lateinit var navController: NavController
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // carichiamo layout
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        val topLevelDestinations = setOf(
+            R.id.homeFragment,
+            R.id.settingsFragment
+        )
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .setDrawerLayout(drawer_layout)
+            .build()
+
+        // Set up ActionBar
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up navigation menu
+        nav_view.setupWithNavController(navController)
+
         observeActions()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -55,12 +94,16 @@ class MainActivity : AppCompatActivity() {
                 true
             }
              */
+            R.id.home -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
             R.id.search_name -> {
                 Timber.d("TI PIACE QUANDO CLICCO IL TUO SEARCH?")
                 true
             }
             R.id.menu_name -> {
-                Toast.makeText(applicationContext, "Work in progress Navigate to Settings", 3000)
+                Toast.makeText(applicationContext, "Work in progress Navigate to Settings", 3000).show()
                 Timber.d("TI PIACE QUANDO CLICCO IL TUO MENU?")
                 mainActivityViewModel.send(MainActivityScreenEvent.OnMenuClick)
                 true
@@ -98,5 +141,27 @@ class MainActivity : AppCompatActivity() {
                 }
             }.exhaustive
         })
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_profile -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_messages -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_friends -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_update -> {
+                Toast.makeText(this, "Update clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(this, "Sign out clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
