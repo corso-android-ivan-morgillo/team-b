3f48fb841feaf9a95a9ceef1fc82cb3bd8a24d6e
Lucrezia Carena <lucreziac@users.noreply.github.com>
2021-03-12 15:48:34 +0100

    fix: clear searchField on No result

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/search/SearchFragment.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.transition.MaterialElevationScale
+import com.ivanmorgillo.corsoandroid.teamb.CleanSearchField
 import com.ivanmorgillo.corsoandroid.teamb.R
 import com.ivanmorgillo.corsoandroid.teamb.databinding.FragmentSearchBinding
 import com.ivanmorgillo.corsoandroid.teamb.search.SearchScreenAction.NavigateToDetail
@@ -72,7 +73,9 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
                     adapter.setSearchList(state.cocktails)
                 }
                 is SearchScreenStates.Error -> {
+                    (activity as CleanSearchField).cleanSearchField()
                     when (state.error) {
+
                         SearchErrorStates.ShowNoInternetMessage -> {
                             errorCustom("No Internet Connection")
                         }
