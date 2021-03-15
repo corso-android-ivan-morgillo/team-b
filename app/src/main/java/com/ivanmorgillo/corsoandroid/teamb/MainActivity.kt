179d8bbbc928234f9b0a6c7a6a43ec8b3869dc23
Lucrezia Carena <lucreziac@users.noreply.github.com>
2021-03-15 11:08:06 +0100

    feat/ add sign-in authentication

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/MainActivity.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ivanmorgillo.corsoandroid.teamb
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -18,7 +19,10 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.apperol.R
 import com.apperol.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.ivanmorgillo.corsoandroid.teamb.MainScreenAction.DisableDarkMode
 import com.ivanmorgillo.corsoandroid.teamb.MainScreenAction.EnableDarkMode
 import com.ivanmorgillo.corsoandroid.teamb.MainScreenAction.NavigateToFacebook
@@ -35,6 +39,8 @@ import timber.log.Timber
 interface CleanSearchField {
     fun cleanSearchField()
 }
+
+private const val RC_SIGN_IN: Int = 1234
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, CleanSearchField {
 
@@ -154,12 +160,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.nav_feedback -> {
                 mainActivityViewModel.send(MainScreenEvent.OnFeedBackClick)
             }
-            R.id.nav_contact -> {
+            R.id.sign_in -> {
                 Timber.d("Contacts")
+                // Choose authentication providers
+                val providers = arrayListOf(
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                )
+                // Create and launch sign-in intent
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                    RC_SIGN_IN
+                )
+                binding.drawerLayout.closeDrawer(GravityCompat.START)
             }
             R.id.nav_randomCocktail -> {
                 mainActivityViewModel.send(MainScreenEvent.OnRandomClick)
             }
+
             /*
             R.id.nav_share -> {
                 Timber.d("Share")
@@ -185,5 +205,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun cleanSearchField() {
         searchView?.setQuery("", false)
         searchView?.isIconified = true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                Timber.d("USER: $user")
+                // ...
+            } else {
+                Timber.e("AUTHENTICATION ERROR: ${response?.error?.errorCode}")
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }
