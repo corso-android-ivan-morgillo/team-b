62303a759e8c43e6e1988f08a60b560daa9abbc6
Santo-G <santo-g@users.noreply.github.com>
2021-03-19 16:22:17 +0100

    feat: added LoggedInUser and LoggedOutUser

--- domain/src/main/java/com/apperol/AuthenticationManager.kt
+++ domain/src/main/java/com/apperol/AuthenticationManager.kt
@@ -1,11 +1,14 @@
 package com.apperol
 
+import com.apperol.User.LoggedInUser
+import com.apperol.User.LoggedOutUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 interface AuthenticationManager {
     suspend fun getUId(): String?
     suspend fun isUserLoggedIn(): Boolean
+    suspend fun getUser(): User
 }
 
 class AuthenticationManagerImpl : AuthenticationManager {
@@ -16,4 +19,21 @@ class AuthenticationManagerImpl : AuthenticationManager {
     override suspend fun isUserLoggedIn(): Boolean {
         return Firebase.auth.currentUser != null
     }
+
+    override suspend fun getUser(): User {
+        return if (isUserLoggedIn()) {
+            LoggedInUser(
+                email = Firebase.auth.currentUser!!.email,
+                name = Firebase.auth.currentUser!!.displayName,
+                image = Firebase.auth.currentUser!!.photoUrl?.toString()
+            )
+        } else {
+            LoggedOutUser
+        }
+    }
+}
+
+sealed class User {
+    data class LoggedInUser(val email: String?, val name: String?, val image: String?) : User()
+    object LoggedOutUser : User()
 }
