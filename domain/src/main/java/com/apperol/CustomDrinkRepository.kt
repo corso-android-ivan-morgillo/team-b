666e4143eb78b179ca2152887cebd8ddb4f769d1
Santo-G <santo-g@users.noreply.github.com>
2021-03-19 15:43:17 +0100

    feat: added custom drink repository

--- domain/src/main/java/com/apperol/CustomDrinkRepository.kt
+++ domain/src/main/java/com/apperol/CustomDrinkRepository.kt
@@ -3,6 +3,8 @@ package com.apperol
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
+import java.math.BigInteger
+import java.util.UUID
 import kotlinx.coroutines.tasks.await
 
 interface CustomDrinkRepository {
@@ -13,12 +15,12 @@ interface CustomDrinkRepository {
 
 class CustomDrinkRepositoryImpl(private val firestore: FirebaseFirestore) : CustomDrinkRepository {
     private val customCollection by lazy {
-        firestore.collection("User - ${getUid()}")
+        firestore.collection("customs")
     }
 
     private fun getUid() = Firebase.auth.currentUser.uid
     override suspend fun save(customDrink: Detail): Boolean {
-        val id = generateRandomUserId() + 1
+        val id = UUID.randomUUID().toString()
         val customMap = hashMapOf(
             "id" to id,
             "name" to customDrink.name,
@@ -28,28 +30,28 @@ class CustomDrinkRepositoryImpl(private val firestore: FirebaseFirestore) : Cust
             "alcoholic" to customDrink.isAlcoholic,
             "ingredients" to customDrink.ingredients,
             "instructions" to customDrink.instructions,
+            "userid" to getUid()
         )
-        customCollection.document("custom-$id").set(customMap).await()
+        customCollection.document(id).set(customMap).await()
         return true
     }
 
     override suspend fun delete(id: Long): Boolean {
-        customCollection.document("custom-$id").delete().await()
+        customCollection.document("$id").delete().await()
         return true
     }
 
     override suspend fun loadAll(): List<Detail>? {
         val customList = customCollection
+            .whereEqualTo("userid", getUid())
             .get()
             .await()
             .documents
-            .filter {
-                it.contains("custom-")
-            }.map {
+            .map {
                 val name = it["name"] as String
                 val image = it["image"] as String
                 val category = it["category"] as String
-                val id = it["id"] as Long
+                val id = it["id"] as String
                 val glass = it["glass"] as String
                 val alcoholic = it["alcoholic"] as Boolean
                 val ingredients = it["ingredients"] as List<Ingredient>
@@ -57,7 +59,7 @@ class CustomDrinkRepositoryImpl(private val firestore: FirebaseFirestore) : Cust
                 Detail(
                     name = name,
                     image = image,
-                    id = id,
+                    id = BigInteger(id.toByteArray()).toLong(),
                     isAlcoholic = alcoholic,
                     glass = glass,
                     ingredients = ingredients,
@@ -71,9 +73,5 @@ class CustomDrinkRepositoryImpl(private val firestore: FirebaseFirestore) : Cust
         } else {
             customList
         }
-    }
-
-    fun generateRandomUserId(): Long {
-        return Firebase.auth.currentUser.uid.hashCode().toLong()
     }
 }
