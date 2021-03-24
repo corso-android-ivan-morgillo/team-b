abffbe316cf06659f1a3ade0abba2b8a4ff5bada
Lucrezia Carena <lucreziac@users.noreply.github.com>
2021-03-24 16:11:49 +0100

    feat: layout upgrade and custom fragment management

--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CustomForm.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CustomForm.kt
@@ -2,10 +2,15 @@ package com.ivanmorgillo.corsoandroid.teamb
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.apperol.R
 import com.apperol.databinding.CustomFormBinding
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormStates.Error
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormStates.Loading
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormStates.Success
 import com.ivanmorgillo.corsoandroid.teamb.utils.bindings.viewBinding
+import com.ivanmorgillo.corsoandroid.teamb.utils.exhaustive
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CustomForm : Fragment(R.layout.custom_form) {
@@ -18,8 +23,44 @@ class CustomForm : Fragment(R.layout.custom_form) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val inputText = binding.customDrinkName.editText?.text.toString()
-
-
+        viewModel.states.observe(viewLifecycleOwner) {
+            when (it) {
+                is Error -> TODO()
+                Loading -> TODO()
+                is Success -> {
+                    binding.customDrinkName.setText(it.content.name)
+                    binding.customDrinkCategory.setText(it.content.type)
+                    binding.customDrinkGlass.setText(it.content.glass)
+                    binding.customDrinkIsAlcoholic.isChecked = it.content.isAlcoholic
+                    it.content.ingredients.forEach {
+                        val textView = TextView(view.context)
+                        textView.text = it
+                        binding.customDrinkIngredientsList.addView(textView)
+                    }
+                    binding.customIngredientName.setText(it.content.ingredientName)
+                    binding.customIngredientQuantity.setText(it.content.ingredientQty)
+                    // INSTRUCTIONS
+                }
+            }.exhaustive
+        }
+        binding.customDrinkAddIngredient.setOnClickListener {
+            val ingredientName = binding.customIngredientName.text.toString()
+            val ingredientQty = binding.customIngredientQuantity.text.toString()
+            // Mandare unità di misura presa dal dialog
+        }
+        binding.customDrinkSave.setOnClickListener { }
+        viewModel.send(CustomFormEvents.OnReady)
     }
 }
+
+data class CustomFormUI(
+    val name: String,
+    val type: String,
+    val isAlcoholic: Boolean,
+    val glass: String,
+    val ingredients: List<String>,
+    val ingredientName: String,
+    val ingredientQty: String,
+    val instructions: String
+
+)
--- app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CustomFormViewModel.kt
+++ app/src/main/java/com/ivanmorgillo/corsoandroid/teamb/CustomFormViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apperol.CustomDrinkRepository
 import com.apperol.Detail
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormEvents.AddIngredient
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormEvents.IsAlcoholicClicked
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormEvents.OnGlassClicked
+import com.ivanmorgillo.corsoandroid.teamb.CustomFormEvents.OnReady
 import com.ivanmorgillo.corsoandroid.teamb.CustomFormEvents.OnSaveClick
 import com.ivanmorgillo.corsoandroid.teamb.detail.DetailErrorStates
 import com.ivanmorgillo.corsoandroid.teamb.utils.Tracking
@@ -23,10 +27,26 @@ class CustomFormViewModel(
     fun send(event: CustomFormEvents) {
         when (event) {
             OnSaveClick -> viewModelScope.launch { saveContent() }
+            OnGlassClicked -> TODO()
+            is IsAlcoholicClicked -> TODO()
+            AddIngredient -> TODO()
+            OnReady -> {
+                val content = CustomFormUI(
+                    name = "PippoDrink",
+                    type = "Ordinary Drink",
+                    isAlcoholic = true,
+                    glass = "flute",
+                    ingredients = listOf("sugar", "vodka", "gin"),
+                    ingredientName = "",
+                    ingredientQty = "",
+                    instructions = "mirko la fa poi"
+                )
+                states.postValue(CustomFormStates.Success(content))
+            }
         }.exhaustive
     }
 
-    private suspend fun saveContent(): Unit {
+    private suspend fun saveContent() {
         states.postValue(CustomFormStates.Loading)
         val custom = drinkCustom ?: return
         customDrinkRepository.save(custom)
@@ -37,9 +57,13 @@ class CustomFormViewModel(
 sealed class CustomFormStates {
     object Loading : CustomFormStates()
     data class Error(val error: DetailErrorStates) : CustomFormStates()
-    object Success : CustomFormStates()
+    data class Success(val content: CustomFormUI) : CustomFormStates()
 }
 
 sealed class CustomFormEvents {
+    class IsAlcoholicClicked(checked: Boolean) : CustomFormEvents()
     object OnSaveClick : CustomFormEvents()
+    object OnGlassClicked : CustomFormEvents()
+    object AddIngredient : CustomFormEvents()
+    object OnReady : CustomFormEvents()
 }
